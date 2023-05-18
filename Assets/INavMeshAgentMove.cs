ac439b74fe93c94b1e50050fc78fb246f1e92080
mpierce1988 <mpierce1138@gmail.com>
2023-05-18 20:40:23 -0300

    Animations now follow user input, not ship motion

--- Assets/INavMeshAgentMove.cs
+++ Assets/INavMeshAgentMove.cs
@@ -7,6 +7,7 @@ public interface INavMeshAgentMove
     float MaxRotationSpeed { get;}
     
     Vector2 Position { get; }
+    Vector2 InputVector { get; }
     Quaternion Rotation { get; }
     bool IsStopped { get; }
     bool CanRotate { get; }
---
+++ Assets/NavEntity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public abstract class NavEntity : MonoBehaviour, INavMeshAgentMove
+{
+	public abstract float MaxSpeed { get; }
+	public abstract Vector3 CurrentVelocity { get; }
+	public abstract float MaxRotationSpeed { get; }
+	public abstract Vector2 Position { get; }
+	public abstract Quaternion Rotation { get; }
+	public abstract bool IsStopped { get; }
+	public abstract bool CanRotate { get; }
+	public abstract float DistanceToDestinationThreshold { get; }
+	public abstract Vector2 InputVector { get; }
+}
